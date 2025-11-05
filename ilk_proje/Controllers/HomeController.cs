ab915826c4fc7297ca9d310d660a60a76cd05068
denizdogruel00 <denizyazilm@gmail.com>
2025-11-05 07:52:12 +0300

    Models klasoru eklendi ve mvc yapısı tamamlandı

--- ilk_proje/Controllers/HomeController.cs
+++ ilk_proje/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ilk_proje.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ilk_proje.Controllers
 {
@@ -18,6 +19,17 @@ namespace ilk_proje.Controllers
         }public string Index4()
         {
             return "merhaba bugün hava çok soğuk";
+        }public IActionResult Index5()
+        {
+            var ktp = new List<Kitap>()
+            {
+                new Kitap(){Id=1,KitapAd="80 Günde devri alem",Yazar="Jules Verne"},
+                new Kitap(){Id=2,KitapAd="Bilinmeyen Bir kadının mektubu",Yazar="zweig" },
+                new Kitap(){Id=3,KitapAd="Bir Noel Hikayesi",Yazar="Charles Dickens" }
+
+            };
+
+            return View(ktp);
         }
     }
 }
---
+++ ilk_proje/Models/Kitap.cs
@@ -0,0 +1,9 @@
+﻿namespace ilk_proje.Models
+{
+    public class Kitap
+    {
+        public int Id { get; set; }
+        public string KitapAd { get; set; }
+        public string Yazar { get; set; }
+    }
+}
